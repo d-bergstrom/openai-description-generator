9f248939cc515b5a184db948032483c911f3acfd
David Bergstrom <davidmbergstrom@gmail.com>
2023-06-06 15:08:22 -0700

    Created Express API

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
+const express = require('express');
+
+//Created functions to query OpenAI
+const { generateDescription, generateImage } = require('./controllers/openaiController');
+
+//Setup express app
+const app = express();
+app.listen(4000, () => console.log('Listening for requests on port 4000'));
+
+//Middleware to extract JSON data from post requests
+//When sending post requests from front end we're sending JSON data in the body of that request
+//This allows us to easily extract that body data to easily pass it to the different route handlers
+app.use(express.json());
+
+//Routes
+//Callback functions accept two arguments (req, res)
+app.post('/openai/description', generateDescription);
+app.post('/openai/image', generateImage);
+
+
+
+
+
+
+
+
+
+/* USER INPUT TESTING
 //Get user input through terminal
 const readline = require('readline');
-
-const { generateDescription, generateImage } = require('./controllers/openaiController');
 
 const rl = readline.createInterface({
     input: process.stdin, //Input will be from keyboard when typing into the terminal
@@ -10,5 +36,6 @@ const rl = readline.createInterface({
 
 //The question method is part of the readline module provided by the readline. Interface class.
 //It has two arguments: A question to the user and a callback function that will be called and passed the user's input as an argument.
-// rl.question('Enter YouTube Video Title: ', generateDescription);
+rl.question('Enter YouTube Video Title: ', generateDescription);
 rl.question('Describe the image: ', generateImage);
+*/
--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,28 +1,39 @@
 /* This file stores functions for querying the OpenAI API */
 
-
 const openai = require('../config/openaiConfig');
 
 //Function that will pass user input to ChatGPT to generate a YouTube video description
-const generateDescription = async (title) => {
+const generateDescription = async (req, res) => {
+    console.log(req);
+    const { title } = req.body; //Descturcture request data due to our already set up middleware
+
     const description = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [{role: "user", content: `Come up with a description for a YouTube video called ${title}`}],
         max_tokens: 100,
     });
 
-    console.log(description.data.choices[0].message);
+    //console.log(description.data.choices[0].message);
+
+    res.status(200).json({
+        description: description.data.choices[0].message
+    });
 }
 
 //Function that will pass user input to ChatGPT to generate an image for a Youtube video description
-const generateImage = async (desc) => {
+const generateImage = async (req, res) => {
+    const { prompt } = req.body; //Descturcture request data due to our already set up middleware
+
     const image = await openai.createImage({
-        prompt: desc,
+        prompt: prompt, //You can also just use req.body.prompt here
         n: 1,
         size: '512x512',
     });
 
-    console.log(image.data.data[0].url);
+    //console.log(image.data.data[0].url);
+    res.json({
+        url: image.data.data[0].url
+    })
 }
 
 module.exports = { generateDescription, generateImage };
